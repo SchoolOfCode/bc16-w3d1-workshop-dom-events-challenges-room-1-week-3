4c3323130674bfc4c1c82ec5c138c4ca39abfa15
moose-hub <infamollagames@gmail.com>
2024-02-20 13:40:10 +0000

    Created 6th custom challenge

---
+++ 06-Room1-TeamChallenge/app.js
@@ -0,0 +1,24 @@
+// Learning Objectives:
+// 1) Build upon the skills you've already worked on
+// - DOM Manipulationm, - Looping, - Utilising Data Structures
+// Challenge:
+// We have a pub quiz form that was left incomplete
+// Using the questions, answers and multiple choice options provided,
+// dynamically fill out the quiz and return an answer
+
+// Expected Outcome:
+// 1) Initially there is an example question structure
+// 2) Your script should only fill the pub quiz sheet with relevant questions
+// from the question data you've been providied.
+// You can choose 5 questions from this data to use, or check bonus objective
+// 3) The submit button should return all selected answers, compare them with
+// the correct answer and display your result in the result div element.
+// example: 5/5 correct!
+// **bonus:**
+// Instead of choosing some questions to use, instead allow the code to do the
+// work for you and have it select 5 random questions every time the quiz is
+// laoded.
+
+import { questions } from "./questions.js";
+
+console.log(questions);
