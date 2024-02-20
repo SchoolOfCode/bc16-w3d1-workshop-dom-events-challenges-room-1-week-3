5001ce10e2627abd1e3f773995c158f3cdd08fd6
moose-hub <54481758+moose-hub@users.noreply.github.com>
2024-02-20 15:54:51 +0000

    Update app.js
    Reformatted instructions to be more clear - challenge 6

--- 06-Room1-TeamChallenge/app.js
+++ 06-Room1-TeamChallenge/app.js
@@ -4,12 +4,12 @@
 // - Looping, 
 // - Utilising and manipulating Data Structures, 
 // - Altering the styles of the page
----------------------------
+// ---------------------------
 // Challenge:
 // We have a pub quiz form that was left incomplete
 // Using the questions, answers and multiple choice options provided,
 // dynamically fill out the quiz and return an answer
----------------------------
+// ---------------------------
 // Expected Outcome:
 // 1) Initially there is an example question structure in the HTML, your script should only fill the pub quiz sheet with relevant questions from the question data you've been providied.
 // You can either choose 5 questions from this data to use, or check the bonus objective for a different way to do it.
@@ -18,7 +18,7 @@
 // 4) Upon submitting the form (questions) should disappear/ no longer be visible, leaving only the result box
 // 5) The players result should be displayed in the result element 
 // example: 5/5 correct!
-----------------------------
+// ----------------------------
 // **bonus objectives:**
 // 1) Instead of choosing some questions to use, code it to pick 5 random
 // questions when the quiz is generated.
@@ -29,7 +29,7 @@
 // 3b) If the player gets 0/5 score
 // - a message should appear challenging the player to do better next time
 // - the result box should change background-color to #f17778
------------------------------
+// -----------------------------
 // ** bonus bonus objective:** 
 // 1) display a play again button after the result is shown, which refreshes the quiz with new questions so the player can try again.
 
