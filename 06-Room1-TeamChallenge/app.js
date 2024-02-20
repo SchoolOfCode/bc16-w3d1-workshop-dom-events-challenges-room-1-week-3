92643ffafe7c4b169d75c84a8e3397c4793554a5
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-20 17:17:38 +0000

    unknown changes

--- 06-Room1-TeamChallenge/app.js
+++ 06-Room1-TeamChallenge/app.js
@@ -1,8 +1,8 @@
 // Learning Objectives:
 // 1) Build upon the skills you've already worked on
-// - DOM Manipulationm, 
-// - Looping, 
-// - Utilising and manipulating Data Structures, 
+// - DOM Manipulationm,
+// - Looping,
+// - Utilising and manipulating Data Structures,
 // - Altering the styles of the page
 // ---------------------------
 // Challenge:
@@ -16,23 +16,22 @@
 // 2) The submit button should return all selected answers.
 // 3) Compare the form data you receive on submit, with the correct answers to generate a correct answers score
 // 4) Upon submitting the form (questions) should disappear/ no longer be visible, leaving only the result box
-// 5) The players result should be displayed in the result element 
+// 5) The players result should be displayed in the result element
 // example: 5/5 correct!
 // ----------------------------
 // **bonus objectives:**
 // 1) Instead of choosing some questions to use, code it to pick 5 random
 // questions when the quiz is generated.
-// 2) 
-// 3a) If the player gets 5/5 score, 
+// 2)
+// 3a) If the player gets 5/5 score,
 // - a message should appear congratulating them below the result
 // - the result box should change background-color to #77f17a
 // 3b) If the player gets 0/5 score
 // - a message should appear challenging the player to do better next time
 // - the result box should change background-color to #f17778
 // -----------------------------
-// ** bonus bonus objective:** 
+// ** bonus bonus objective:**
 // 1) display a play again button after the result is shown, which refreshes the quiz with new questions so the player can try again.
 
 import { questions } from "./questions.js";
-
 console.log(questions);
